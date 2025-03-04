8069568513b6e7ad0fddb21d82d45ee3b156a5a1
jenri019 <j.enrique209436@hotmail.com>
2025-03-04 17:16:36 -0600

    feat: Modificando el moelo

--- back/home/models.py
+++ back/home/models.py
@@ -1,9 +1,10 @@
 from django.db import models
-from django.db.models.fields import CharField, TextField, URLField, IntegerField, BooleanField
+from django.db.models.fields import CharField, TextField, URLField, IntegerField, BooleanField, AutoField
 from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Item(models.Model):
+    id = AutoField(primary_key=True)
     title = CharField(max_length=100)
     description = TextField(max_length = 300, blank=True)
     url = URLField()
