b349108c6361cce0409a32a2d72d1cc2edaaa465
jenri019 <j.enrique209436@hotmail.com>
2025-03-04 17:29:52 -0600

    feat: Modificando el modelo pra añadir generos

--- back/home/models.py
+++ back/home/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.db.models.fields import CharField, TextField, URLField, IntegerField, BooleanField, AutoField
-from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Item(models.Model):
@@ -10,7 +9,4 @@ class Item(models.Model):
     url = URLField()
     current_chapter = IntegerField()
     on_going = BooleanField(default=True)
-    generos = ArrayField(
-        CharField(max_length=50),
-        default=list
-    )
+    generos = models.TextField(default="")
