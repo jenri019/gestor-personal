65f320e94ada8e0a29d971f2b6532a355c9dde82
jenri019 <j.enrique209436@hotmail.com>
2025-03-05 15:38:04 -0600

    feat: Añaadiendo un headder y mostrando el popup

--- front/src/components/CardContainer.component.jsx
+++ front/src/components/CardContainer.component.jsx
@@ -1,24 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Card from './card.component'
+import Popup from './Popup.component';
 
 const handleButtonClick = (url) => {
     window.open(url, '_blank');
 }
 
 const CardContainer = ({ items }) => {
+    const [showPopup, setShowPopup] = useState(false);
+
+    const handleButtonClick = (url) => {
+        window.open(url, '_blank');
+    };
+
+    const handleShowPopup = () => {
+        setShowPopup(true);
+    };
+
+    const handleClosePopup = () => {
+        setShowPopup(false);
+    };
+
     return (
-        <div className='card-container'>
-            {items.map(({ id, title, current_chapter, description, generos, url }) => (
-                <Card
-                    key={id}
-                    title={title}
-                    description={description}
-                    generos={generos}
-                    current_chapter={current_chapter}
-                    onButtonClick={() => handleButtonClick(url)} 
-                />
-            ))}
-        </div>
+        <>
+            <button onClick={handleShowPopup}>Agregar</button>
+            <Popup show={showPopup} onClose={handleClosePopup}>
+                <h2>Contenido del Popup</h2>
+                <p>Este es el contenido del popup.</p>
+            </Popup>
+            <div className='card-container'>
+                {items.map(({ id, title, current_chapter, description, generos, url }) => (
+                    <Card
+                        key={id}
+                        title={title}
+                        description={description}
+                        generos={generos}
+                        current_chapter={current_chapter}
+                        onButtonClick={() => handleButtonClick(url)}
+                    />
+                ))}
+            </div>
+        </>
     )
 }
 
--- front/src/components/Navbar.component.jsx
+++ front/src/components/Navbar.component.jsx
@@ -3,15 +3,9 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <Link to="/home">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-            </ul>
+        <nav className='header'>
+            <Link to="/home">Home</Link>
+            <Link to="/about">About</Link>
         </nav>
     );
 };
