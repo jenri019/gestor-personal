44a0d8076d502154a9362f47120112a88892bd3d
jenri019 <j.enrique209436@hotmail.com>
2025-03-05 10:46:32 -0600

    feat: Añadiendo el capitulo actual

--- front/src/components/Card.component.jsx
+++ front/src/components/Card.component.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import Description from './Description.component'
 import Genres from './Genres.component'
 
-const Card = ({title, description, generos}) => {
+const Card = ({title, current_chapter, description, generos, onButtonClick}) => {
   return (
     <div className='card'>
         <h1 className="title">
-            {title}
+            {title} ({current_chapter})
         </h1>
         <Description description={description} />
         <Genres generos={generos} />
-        <button type="button" className='btn btn-submit'>Ir a cap</button>
+        <button type="button" className='btn btn-primary' onClick={onButtonClick}>Ir a cap</button>
     </div>
   )
 }
--- front/src/components/CardContainer.component.jsx
+++ front/src/components/CardContainer.component.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import Card from './card.component'
 
-const CardContainer = ({items}) => {
+const handleButtonClick = (url) => {
+    window.open(url, '_blank');
+}
+
+const CardContainer = ({ items }) => {
     return (
         <div className='card-container'>
-            {items.map(({id, title, description, generos}) => (
-                <Card key={id} title={title} description={description} generos={generos} />
+            {items.map(({ id, title, current_chapter, description, generos, url }) => (
+                <Card
+                    key={id}
+                    title={title}
+                    description={description}
+                    generos={generos}
+                    current_chapter={current_chapter}
+                    onButtonClick={() => handleButtonClick(url)} 
+                />
             ))}
         </div>
     )
