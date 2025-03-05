a94b11c6133cbef9a66e30184a1fe2e3fcab142c
jenri019 <j.enrique209436@hotmail.com>
2025-03-05 10:51:44 -0600

    feat: Añadiendo el tipo al modelo

--- back/home/models.py
+++ back/home/models.py
@@ -5,6 +5,7 @@ from django.db.models.fields import CharField, TextField, URLField, IntegerField
 class Item(models.Model):
     id = AutoField(primary_key=True)
     title = CharField(max_length=100)
+    tipo = CharField(max_length=6, default="")
     description = TextField(max_length = 300, blank=True)
     url = URLField()
     current_chapter = IntegerField()
